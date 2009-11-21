1eb8350412a2f7b42da132d9dc6ac9318901ce70
Phlip <phlip2005@gmail.com>
2009-11-20 16:36:20 -0800

    developing

--- morelia/morelia.py
+++ morelia/morelia.py
@@ -99,10 +99,10 @@ class TestVisitor:
 
 class Morelia:
         
-    def __init__(self, predicate = '', list = []):  #  TODO  take these out
+    def __init__(self): # , predicate = '', list = []):  #  TODO  take these out
         self.parent = None
-        if predicate != '':  print 'TODO'
-        self._parse(predicate, list)  #  TODO  list -> Parser
+        #~ if predicate != '':  print 'TODO'
+        #~ self._parse(predicate, list)  #  TODO  list -> Parser
 
     def _parse(self, predicate, list = []):
         self.concept = self.my_class_name()

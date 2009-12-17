13003e320aacb362b58acf711e21d7a9cf5fcaae
Phlip <phlip2005@gmail.com>
2009-12-17 15:51:45 -0800

    developing

--- tests/morelia_suite.py
+++ tests/morelia_suite.py
@@ -464,7 +464,7 @@ class MoreliaSuite(TestCase):
         try:
             statements = statements.replace('\\n', '\n')  #  TODO  document this is how you paint linefeedage
             statements = statements.replace('\\', '')  #  TODO  document this is how you paint reserved words
-            diagnostics = diagnostics.replace('\\', '')  #  TODO  document this is how you paint reserved words
+            diagnostics = diagnostics.replace('\\', '')  #  TODO  document this is how you escape pipes
             p = Parser().parse_features(statements)
             p.evaluate(self)
             assert False  #  we expect syntax errors here

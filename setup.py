2e34406a5694cdfc63762eacd6fbd1284ada532c
Phlip <phlip2005@gmail.com>
2009-12-22 12:26:44 -0800

    developing

--- setup.py
+++ setup.py
@@ -4,10 +4,24 @@
 from distutils.core import setup
 
 setup(name='Morelia',
-      version='0.0.3',
+      version='0.0.4',
       description='squeeze your features',
       author='Phlip',
       author_email='phlip2005@gmail.com',
       url='TODO',
       packages=['morelia'],
+#    packages = find_packages('src'),
+    package_dir = {'':''},
+    keywords = "django testing bdd",
+    install_requires=[],
+    classifiers = [
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Testing',
+    ]
+
+
+
      )

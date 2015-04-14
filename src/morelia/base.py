724f180485a1eeb3efc9cc75e2ce349aa2999a57
dryobates <jakub.stolarski@kidosoft.pl>
2015-04-14 21:33:13 +0200

    Bump version: 0.2.1 → 0.3.0

--- src/morelia/base.py
+++ src/morelia/base.py
@@ -21,7 +21,7 @@ from .utils import to_unicode
 from .visitors import TestVisitor, ReportVisitor, StepMatcherVisitor
 
 
-__version__ = '0.2.1'
+__version__ = '0.3.0'
 
 
 #  TODO  what happens with blank table items?

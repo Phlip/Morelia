be0e60b42250467df6adc873c2287a4b2c5b7930
dryobates <jakub.stolarski@kidosoft.pl>
2015-04-26 20:50:02 +0200

    Bump version: 0.3.0 → 0.4.0

--- src/morelia/__init__.py
+++ src/morelia/__init__.py
@@ -4,7 +4,7 @@ import sys
 from .formatters import PlainTextFormatter, ColorTextFormatter
 from .parser import Parser  # noqa
 
-__version__ = '0.3.0'
+__version__ = '0.4.0'
 
 
 def has_color_support():

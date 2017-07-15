3c3f0093630589b6b46345e0c46089c81641cb2b
dryobates <jakub.stolarski@gmail.com>
2017-07-15 17:24:30 +0200

    Simplified comments feature.

--- tests/features/comments.feature
+++ tests/features/comments.feature
@@ -7,22 +7,12 @@ Feature: Comments support
 
     Scenario: Comment can be put on separate line after any step
         Given I have some comment after step on separate line
-        # like this this one
+        # like this one
         Then scenario will pass
-
-    Scenario: Comment can be put after row in table on the same line
-        Given I have some comment after row in table
-        And I have interpolated <data> from table
-        When I execute this scenario
-        Then scenario will pass
-            | data      | # this is unusual comment, as it can be put in the same line as data row
-            | some data |
 
     Scenario: Comment can be put between table rows
-        Given I have some comment after step without table
-        And I have interpolated <data> from table
-        When I execute this scenario
-        Then scenario will pass
+        Given I have table with comment between rows
             | data      |
             # this is a comment between table rows
             | some data |
+        Then I won't have comment in interpolated <data> from table
--- tests/test_comments.py
+++ tests/test_comments.py
@@ -15,26 +15,15 @@ class CommentsTest(TestCase):
         filename = os.path.join(pwd, 'features/comments.feature')
         run(filename, self)
 
-    def step_I_have_some_comment_after_step_without_table(self):
-        r'I have some comment after step without table'
-        pass
-
-    def step_I_have_interpolated_data_from_table(self, data):
-        r'I have interpolated (.+) from table'
-        self.assertNotIn('#', data)
-
-    def step_I_execute_this_scenario(self):
-        pass
-
     def step_scenario_will_pass(self):
         assert True
 
     def step_I_have_some_comment_after_step_on_separate_line(self):
         pass
 
-    def step_I_have_some_comment_after_step_in_the_same_line_like_this_this_one(self):
-        r'I have some comment after step in the same line'
+    def step_I_have_table_with_comment_between_rows(self):
         pass
 
-    def step_I_have_some_comment_after_row_in_table(self):
-        pass
+    def step_I_won_t_have_comment_in_interpolated_data_from_table(self, data):
+        r'I won\'t have comment in interpolated (.+) from table'
+        self.assertNotIn('#', data)

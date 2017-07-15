ff0f28a13be574da204d301fcc2deef9ae02d6a3
dryobates <jakub.stolarski@gmail.com>
2017-07-15 18:44:46 +0200

    Simplified background feature tests.

--- tests/features/background.feature
+++ tests/features/background.feature
@@ -1,36 +1,31 @@
 Feature: Background support
 
+     Morelia processes this prose and runs the results as 
+     a test suite. This prose describes how background syntax can be used.
+
     Background:
-        Given step_ran was "1"
-        And alt_step_ran was "0"
-        And angles_step was <background>
-        And background_only_step_ran incremented
+        Given I have some background steps defined
+        And step contains <when>
 
-    Scenario: execute background steps
-        When I increment step_ran by "1"
-        And increment alt_step_ran by "1"
-        Then step_ran will equal "2"
-        And alt_step_ran will equal "1"
-        Then angles_step will be string "<background>"
+    Scenario: background steps are executed before scenario steps
+        When scenario is executed
+        Then all background steps are executed before any step defined in scenario
 
-    Scenario: execute background steps with second scenario
-        When I increment step_ran by "1"
-        And increment alt_step_ran by "1"
-        Then step_ran will equal "2"
-        And alt_step_ran will equal "1"
+    Scenario: background steps are executed before every scenario
+        When other scenario is executed
+        Then background steps are executed again before every scenario
 
-    Scenario: execute background steps and scenario's givens
-        Given alt_step_ran was "2"
-        When I increment step_ran by "1"
-        And increment alt_step_ran by "1"
-        Then step_ran will equal "2"
-        And alt_step_ran will equal "3"
+    Scenario: background steps and scenario's givens
+        Given single scenario given step
+        When scenario is executed
+        Then scenario given step is executed after background steps
 
     Scenario: execute background steps with table
-        When I increment angles_step by <when>
-        Then angles_step will equal <then>
-            | background | when | then |
-            | 1          | 2    | 3    |
+        When scenario is executed
+        Then background step with <when> will be executed
+            | when          |
+            | some value    |
 
     Scenario: background steps should be run once per scenario
-        Then background_only_step_ran will equal scenarios number
+        When all scenarios are executed
+        Then background steps will be executed once per every scenario case
--- tests/test_background.py
+++ tests/test_background.py
@@ -12,71 +12,65 @@ pwd = os.path.dirname(os.path.realpath(__file__))
 @tags(['acceptance'])
 class BackgroundTest(TestCase):
 
+    def setUp(self):
+        self.__steps_executed = []
+
     def test_background(self):
         filename = os.path.join(pwd, 'features/background.feature')
         ast = Parser().parse_file(filename)
-        self._scenarios_num = sum(1 for s in ast.steps if isinstance(s, Scenario))
+        self.__scenarios_num = sum(1 for s in ast.steps if isinstance(s, Scenario))
         ast.evaluate(self)
 
-    def step_step_ran_is_number(self, number):
-        r'step_ran was "([^"]+)"'
-
-        self._step_ran = int(number)
-
-    def step_alt_step_ran_is_number(self, number):
-        r'alt_step_ran was "([^"]+)"'
-
-        self._alt_step_ran = int(number)
-
-    def step_I_increment_step_ran_by_number(self, number):
-        r'I increment step_ran by "([^"]+)"'
-
-        self._step_ran += int(number)
-
-    def step_increment_alt_step_ran_by_number(self, number):
-        r'increment alt_step_ran by "([^"]+)"'
-
-        self._alt_step_ran += int(number)
-
-    def step_step_ran_equals_number(self, number):
-        r'step_ran will equal "([^"]+)"'
-
-        self.assertEqual(self._step_ran, int(number))
-
-    def step_alt_step_ran_equals_number(self, number):
-        r'alt_step_ran will equal "([^"]+)"'
-
-        self.assertEqual(self._alt_step_ran, int(number))
-
-    def step_angles_step_is_background(self, background):
-        r'angles_step was (.+)'
-
-        self._background = background
-
-    def step_I_increment_angles_step_by_when(self, when):
-        r'I increment angles_step by (.+)'
-
-        self._background = int(self._background) + int(when)
-
-    def step_angles_step_equals_then(self, then):
-        r'angles_step will equal (.+)'
-
-        self.assertEqual(self._background, int(then))
-
-    def step_angles_step_will_be_then(self, then):
-        r'angles_step will be string "([^"]+)"'
-
-        self.assertEqual(self._background, then)
-
-    def step_background_only_step_ran_incremented(self):
-        r'background_only_step_ran incremented'
-
+    def step_I_have_some_background_steps_defined(self):
+        self.__steps_executed.append('I have some background steps defined')
         try:
-            self._background_only_step_ran += 1
+            self.__background_step_ran += 1
         except AttributeError:
-            self._background_only_step_ran = 1
+            self.__background_step_ran = 1
 
-    def step_background_only_step_ran_will_equal_scenarios_number(self):
-        r'background_only_step_ran will equal scenarios number'
+    def step_scenario_is_executed(self):
+        self.__steps_executed.append('scenario is executed')
 
-        self.assertEqual(self._background_only_step_ran, self._scenarios_num)
+    def step_all_background_steps_are_executed_before_any_step_defined_in_scenario(self):
+        background_step = 'I have some background steps defined'
+        scenario_step = 'scenario is executed'
+        msg = 'Background step not executed before scenario steps'
+        self.__assert_background_step_executed_before_scenario_step(background_step, scenario_step, msg)
+
+    def step_other_scenario_is_executed(self):
+        self.__steps_executed.append('other scenario is executed')
+
+    def step_background_steps_are_executed_again_before_every_scenario(self):
+        background_step = 'I have some background steps defined'
+        scenario_step = 'other scenario is executed'
+        msg = 'Background step not executed before scenario steps'
+        self.__assert_background_step_executed_before_scenario_step(background_step, scenario_step, msg)
+
+    def step_single_scenario_given_step(self):
+        self.__steps_executed.append('single scenario given step')
+
+    def step_scenario_given_step_is_executed_after_background_steps(self):
+        background_step = 'I have some background steps defined'
+        scenario_step = 'single scenario given step'
+        msg = 'Background step not executed before scenario given step'
+        self.__assert_background_step_executed_before_scenario_step(background_step, scenario_step, msg)
+
+    def __assert_background_step_executed_before_scenario_step(self, background_step, scenario_step, msg):
+        background_step_idx = self.__steps_executed.index(background_step)
+        scenario_given_step_idx = self.__steps_executed.index(scenario_step)
+        self.assertLess(background_step_idx, scenario_given_step_idx, msg=msg)
+
+    def step_step_contains_angle_variable(self, angle_variable):
+        r'step contains (.+)'
+        self.__angle_variable = angle_variable
+
+    def step_background_step_with_angle_variable_will_be_executed(self, angle_variable):
+        r'background step with (.+) will be executed'
+        self.assertEqual(angle_variable, self.__angle_variable)
+
+    def step_all_scenarios_are_executed(self):
+        pass
+
+    def step_background_steps_will_be_executed_once_per_every_scenario_case(self):
+        self.assertLessEqual(self.__scenarios_num, self.__background_step_ran, msg='Background step not executed for every scenario')
+        self.assertGreaterEqual(self.__scenarios_num, self.__background_step_ran, msg='Background step executed more then once for every scenario')
